1c95ce3c8537a20d549b5b7a2c6c3f4a649a8221
EduardoAlbert <albert-costa1@live.com>
2019-10-01 11:16:45 -0300

    session controll

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-ixnbf.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 // GET, POST, PUT, DELETE
 
