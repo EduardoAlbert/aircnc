530c5af0c3945e732db8b2ebc3f2279be3916186
EduardoAlbert <albert-costa1@live.com>
2019-10-02 18:18:13 -0300

    routes front-end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
 const routes = require('./routes');
 
 const app = express();
@@ -15,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-ixnbf.mongodb.net
 // req.params = Acessar route params (para edição ou delete)
 // req.body = Acessar corpo da requisição (para criação ou edição)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import './App.css';
 
 import logo from './assets/logo.svg'
+
+import Routes from './routes';
 
 function App() {
   return (
@@ -9,21 +12,7 @@ function App() {
       <img src={logo} alt="AirCnC"/>
     
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-        
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            id="email" 
-            type="email" 
-            placeholder="Seu melhor e-mail"
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
-
+        <Routes />
       </div>
     </div>
   );
