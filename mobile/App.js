70071eb607fb77b5800e9933b25d2a358597edfa
EduardoAlbert <albert-costa1@live.com>
2019-10-03 13:35:48 -0300

    init mobile

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <>
+      <View>
+        <Text>Hello Omnistack</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
