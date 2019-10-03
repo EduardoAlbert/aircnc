e8cb25e87ec2170aaa076f125291b7b25e6c03ed
EduardoAlbert <albert-costa1@live.com>
2019-10-03 19:43:32 -0300

    list route

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,6 +1,80 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import {  View, Text, StyleSheet, AsyncStorage, TextInput, TouchableOpacity } from 'react-native';
 
-export default function Book() {
-  return <View />
+import api from '../services/api'
+
+export default function Book({ navigation }) {
+  const [date, setDate] = useState('');
+  const id = navigation.getParam('id');
+
+  async function handleSubmit() {
+    const user_id = await AsyncStorage.getItem('user')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Qual data você quer reservar?"
+      placeholderTextColor="#999"
+      autoCapitalize="words"
+      autoCorrect={false}
+      value={date}
+      onChangeText={setDate}
+      />
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={handleSubmit} style={[styles.button ,styles.cancelButton]}>
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+    marginTop: 50,
+  },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 30,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2,
+  },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: "center",
+    borderRadius: 2,
+  },
+
+  cancelButton: {
+    backgroundColor: '#ccc',
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold', 
+    fontSize: 16
+  }
+})
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png'
 
 export default function List() {
-  return <View />
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image  style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => <SpotList tech={tech} />)}
+      </ScrollView>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10,
+  }
+});
